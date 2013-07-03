4312584c13cd324b598449070f5b2380d0404656
Evan Kuchar <evan.kuchar@gmail.com>
2013-07-02 19:24:41 -0700

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chattr::Application.config.secret_key_base = '665f3b1325b861fc802e05216d3a0a37bb1a46631e3332b37fb4e17e4e14514b0cf64e244d25beef08d10297a4efa5fbe187e3bec8c85585dca11d7cbbf2a90d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Chattr::Application.config.session_store :cookie_store, key: '_chattr_session'
